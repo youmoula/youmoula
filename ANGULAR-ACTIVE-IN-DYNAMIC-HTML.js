703409c8d7f403503ebcab9c76d100d41195cda3
youmoula <rain.private@gmail.com>
2017-04-01 12:41:08 +0800

    Update and rename ANGULAR-ACTIVE-IN-DYNAMIC-HTML to ANGULAR-ACTIVE-IN-DYNAMIC-HTML.js

--- ANGULAR-ACTIVE-IN-DYNAMIC-HTML.js
+++ ANGULAR-ACTIVE-IN-DYNAMIC-HTML.js
@@ -1,10 +1,10 @@
 ## ANGULAR MODULE NGAPP,CONTROLLER NGCONTROLLER
-`var scope = angular.element($(".ngcontroller")).scope();`
+var scope = angular.element($(".ngcontroller")).scope();
 
-`
+
 angular.element($("#ngapp")).injector().invoke(function($compile) {
   var scope = angular.element($(".ngcontroller")).scope();
   $compile($(".ngcontroller").contents())(scope);
   scope.$apply();
 });
-`
+
